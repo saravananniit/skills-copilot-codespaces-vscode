84b385ecc34db7578bf4aced7580bbd3ed6d5a77
saravananP <sarmca2009@gmail.com>
2025-02-19 03:23:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    type: 'member', // member type
+    skills: ['JavaScript', 'React', 'Node.js', 'MongoDB', 'Express.js'],
+    };  
+}
+
+
